3dc6ef2a0a2497b59bb326884e77d7f92cfa1e74
Caleb Wang <cw@berkeley.edu>
2013-01-14 14:53:11 -0500

    change base url

--- amazon.py
+++ amazon.py
@@ -3,7 +3,7 @@ import cookielib
 import selenium
 
 class AmazonSession:
-    login_url = 'https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin'
+    url = 'http://www.amazon.com'
 
     def __init__(self):
         self.br = Browser(factory = mechanize.RobustFactory())
@@ -13,9 +13,11 @@ class AmazonSession:
         self.ff = selenium.webdriver.Firefox()
 
     def login_mechanize(self, email, password):
-        self.br.open(login_url)
+        self.br.open(url)
+        self.br.follow_link(text_regex='sign in')
         br.select_form(nr=0)
         br['email'] = email
+        br['create'] = '0'
         br['password'] = password
 
     def login_selenium_ff(self, email, password):
