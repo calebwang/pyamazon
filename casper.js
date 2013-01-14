23c67b1533ba8007d6d42fbe7c89070310957c67
Caleb Wang <cw@berkeley.edu>
2013-01-14 17:50:51 -0500

    casper working. user-agent change

--- casper.js
+++ casper.js
@@ -1,7 +1,7 @@
 var url = 'https://www.amazon.com';
 var casper = require('casper').create();
 
-phantom.cookiesEnabled = true;
+casper.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.6 Safari/537.11");
 
 casper.start(url, function(){
     this.echo(this.getCurrentUrl());
@@ -21,8 +21,8 @@ casper.then(function() {
 
 casper.thenEvaluate(function() {
 
-    document.querySelector('#ap_email').value = 'my_email';
-    document.querySelector('#ap_password').value = 'my_email';
+    document.querySelector('#ap_email').value = 'calebd.wang@gmail.com';
+    document.querySelector('#ap_password').value = 'hunter2';
     // document.querySelector('#signInSubmit').click();
     document.querySelector('#ap_signin_form').submit();
 
