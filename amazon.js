3702471a6b79b11b89dad78005a11395407ef9ae
Caleb Wang <cw@berkeley.edu>
2013-01-14 16:34:28 -0500

    using casper

---
+++ amazon.js
@@ -0,0 +1,22 @@
+var url = 'https://www.amazon.com';
+var casper = require('casper').create();
+
+casper.start(url, function(){
+    this.echo(this.getCurrentUrl());
+    this.capture('1.png');
+    this.click('sign in');
+});
+
+casper.then(function() {
+    this.echo(this.getCurrentUrl());
+    this.capture('2.png');
+    this.fill('form#ap_signin_form', { email: 'my_email',
+                                       password: 'my_password' }, true);
+}
+
+casper.then(function() {
+
+    this.echo(this.getCurrentUrl());
+    this.capture('3.png');
+}
+casper.run();
